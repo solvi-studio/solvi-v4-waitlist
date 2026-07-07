e208a00134759f5d8a085f469785b4bdf5a67a7b
phungdang2004@gmail.com <phungdang2004@gmail.com>
2026-07-07 15:41:18 +1000

    feat: add /waitlist page with Clerk Waitlist

---
+++ app/waitlist/page.tsx
@@ -0,0 +1,9 @@
+import { Waitlist } from "@clerk/nextjs";
+
+export default function WaitlistPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-6">
+      <Waitlist />
+    </main>
+  );
+}
